a69ac45fcf38fd622192e52f0554d761701d5159
NawImNate <jnhatch3@gmail.com>
2022-07-18 11:58:38 -0400

    importing packages

--- api_node/index.js
+++ api_node/index.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const helmet = require("helmet");
+
+app.listen(8800, () => {
+  console.log(`Server running on 8800`);
+});
