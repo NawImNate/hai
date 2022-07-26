22ed8ba2759ab9fedf02278d68c1caf21eb04880
NawImNate <jnhatch3@gmail.com>
2022-07-26 15:29:03 -0400

    CRUD users express

---
+++ api_node/routes/users.js
@@ -0,0 +1,10 @@
+const User = require("../models/User");
+const router = require("express").Router();
+
+// update users
+// delete users
+// get users
+// follow a user
+// unfollow a user
+
+module.exports = router;
