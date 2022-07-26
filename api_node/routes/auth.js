7068b7117800899b4cfbd67df85e6d8fd257a68d
NawImNate <jnhatch3@gmail.com>
2022-07-26 12:42:06 -0400

    registration test success, passwords encrypt

--- api_node/routes/auth.js
+++ api_node/routes/auth.js
@@ -1,7 +1,26 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-  res.send("Hey its auth routes");
+// Register
+router.post("/register", async (req, res) => {
+  try {
+    // Generating a PW
+    const salt = await bcrypt.genSalt(10);
+    // Encrypt a password
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    // newUser object values to post, pulling from json body from client.
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    // Save user and return http response.
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
