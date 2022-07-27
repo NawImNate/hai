2a2791f1ca9ae0edd9081fcce22c94f2c19abaef
NawImNate <jnhatch3@gmail.com>
2022-07-27 12:16:41 -0400

    deleted user route

--- api_node/routes/user.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-// this file will contain all the references for the index file to refer to for all the routes needed for users, to delete, edit and create them.
-
-router.get("/", (req, res) => {
-  res.send("Hey its user routes");
-});
-
-module.exports = router;
