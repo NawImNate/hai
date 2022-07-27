c253116e7f033c144a6447d2be2fc3b20bebda59
NawImNate <jnhatch3@gmail.com>
2022-07-27 16:21:16 -0400

    successfully adding followers and following

--- api_node/routes/users.js
+++ api_node/routes/users.js
@@ -59,9 +59,25 @@ router.get("/:id", async (req, res) => {
   }
 });
 // follow a user
-// router.("/", async(req,res) =>{
-
-// })
+router.put("/:id/follow", async (req, res) => {
+  if (req.body._id !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.id);
+      if (!user.followers.includes(req.body._id)) {
+        await user.updateOne({ $push: { followers: req.body.id } });
+        await currentUser.updateOne({ $push: { following: req.params.id } });
+        res.status(200).json("now following");
+      } else {
+        res.status(403).json("already following");
+      }
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("you can only follow others.");
+  }
+});
 // unfollow a user
 // router.("/", async(req,res) =>{
 
