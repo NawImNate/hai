9b5802367884a9d960475306729399d9431ecfa1
NawImNate <jnhatch3@gmail.com>
2022-07-29 14:18:21 -0400

    can successfully delete correct posts

--- api_node/routes/posts.js
+++ api_node/routes/posts.js
@@ -19,20 +19,36 @@ router.put("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
 
-    if (post.userId === req.body.id) {
-      // destructure the object and make it exclude the users id for updatingone ? ? ?
-      // the post will update but unfortunately the userID can also be manipulated in the database, pull ticket for improvement later
+    if (post.userID === req.body.userID) {
       await post.updateOne({ $set: req.body });
       res.status(200).json("post updated");
-      res.status(200).json(other);
     } else {
-      res.status(403).json("you can only update your post");
+      res.status(403).json("you can only update your post(s)");
     }
   } catch (err) {
     res.status(500).json(err);
   }
 });
 // delete a post
+router.delete("/:id", async (req, res) => {
+  // create a variable for a post and find the post by id of params
+  try {
+    const post = await Post.findById(req.params.id);
+    // identify if post is eligible for deletion
+    if (post.userID === req.body.userID) {
+      // delete post
+      await post.deleteOne();
+      // success
+      res.status(200).json("post deleted");
+    } else {
+      // if if statement was not true, resulting in an error
+      res.status(403).json("you can only delete your post(s)");
+    }
+  } catch (err) {
+    // if entire try block didnt execute
+    res.status(500).json(err);
+  }
+});
 // like a post
 // get a post
 // get all posts
