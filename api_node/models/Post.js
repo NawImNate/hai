1a6131c07a9f9bbe23895623772eecc97f4ca865
NawImNate <jnhatch3@gmail.com>
2022-07-29 11:43:28 -0400

    defined Post schema

---
+++ api_node/models/Post.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+  {
+    userID: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      max: 500,
+    },
+    img: {
+      type: String,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+  },
+  // automatically update
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", PostSchema);
