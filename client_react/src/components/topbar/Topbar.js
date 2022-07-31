1ef33718dd6544d2779593a6b3247fce966add7c
NawImNate <jnhatch3@gmail.com>
2022-07-31 18:13:44 -0400

    topbar building

--- client_react/src/components/topbar/Topbar.js
+++
@@ -1,7 +0,0 @@
-import "./topbar.css";
-
-const topbar = () => {
-  return <div>topbar</div>;
-};
-
-export default topbar;
--- client_react/src/pages/home/Home.js
+++ client_react/src/pages/home/Home.js
@@ -1,7 +1,7 @@
-import React from "react";
+import Topbar from "../../components/topbar/Topbar";
 
 const Home = () => {
-  return <div>Home Page</div>;
+  return <Topbar />;
 };
 
 export default Home;
