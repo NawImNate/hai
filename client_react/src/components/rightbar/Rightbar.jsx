774ad11e3fd0fe9faf2165ea138206ed72c9f022
NawImNate <jnhatch3@gmail.com>
2022-08-01 17:25:13 -0400

    made birthdays section, next is to style it

--- client_react/src/components/rightbar/Rightbar.jsx
+++ client_react/src/components/rightbar/Rightbar.jsx
@@ -1,7 +1,22 @@
 import "./rightbar.css";
 
 const Rightbar = () => {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img
+            className="birthdayImg"
+            src="assets/gift.png"
+            alt="birthday icon"
+          />
+          <div className="birthdayText">
+            <b>Zach Everett</b> and <b>2 other friends</b> have birthday's today
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Rightbar;
