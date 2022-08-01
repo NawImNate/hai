233e5c11b91872b576ad8cb32fdf3b871eb8e057
NawImNate <jnhatch3@gmail.com>
2022-08-01 11:47:52 -0400

    imported share component into feed component

--- client_react/src/components/feed/Feed.jsx
+++ client_react/src/components/feed/Feed.jsx
@@ -1,7 +1,14 @@
+import Share from "../share/Share";
 import "./feed.css";
 
 const Feed = () => {
-  return <div className="feed">Feed</div>;
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+      </div>
+    </div>
+  );
 };
 
 export default Feed;
