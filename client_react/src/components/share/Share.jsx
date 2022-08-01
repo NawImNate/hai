05be778f765fa55fa45efbc4dac083ff89630d5c
NawImNate <jnhatch3@gmail.com>
2022-08-01 11:47:30 -0400

    made share component

---
+++ client_react/src/components/share/Share.jsx
@@ -0,0 +1,14 @@
+import "./share.css";
+
+const Share = () => {
+  return (
+    <div className="share">
+      <div className="shareWrapper">
+        <div className="shareTop"></div>
+        <div className="shareBottom"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Share;
