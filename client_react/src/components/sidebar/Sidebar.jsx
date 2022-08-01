4f55186c3419bc23dddc1e19a9f353f1c9e8dc60
NawImNate <jnhatch3@gmail.com>
2022-07-31 21:19:42 -0400

    integrated mui icons into sidebar

--- client_react/src/components/sidebar/Sidebar.jsx
+++ client_react/src/components/sidebar/Sidebar.jsx
@@ -1,12 +1,55 @@
 import "./sidebar.css";
 import RssFeedIcon from "@mui/icons-material/RssFeed";
+import PlayCircleFilledOutlinedIcon from "@mui/icons-material/PlayCircleFilledOutlined";
+import ForumIcon from "@mui/icons-material/Forum";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
+import EventIcon from "@mui/icons-material/Event";
+import SchoolIcon from "@mui/icons-material/School";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebarWrapper">
         <ul className="sidebarList">
-          <li className="sidebarListItem"></li>
+          <li className="sidebarListItem">
+            <RssFeedIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <ForumIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Message</span>
+          </li>
+          <li className="sidebarListItem">
+            <PlayCircleFilledOutlinedIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Videos</span>
+          </li>
+          <li className="sidebarListItem">
+            <PeopleOutlinedIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Groups</span>
+          </li>
+          <li className="sidebarListItem">
+            <BookmarkIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Bookmarks</span>
+          </li>
+          <li className="sidebarListItem">
+            <HelpOutlineIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Questions</span>
+          </li>
+          <li className="sidebarListItem">
+            <WorkOutlineIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Jobs</span>
+          </li>
+          <li className="sidebarListItem">
+            <EventIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Events</span>
+          </li>
+          <li className="sidebarListItem">
+            <SchoolIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Courses</span>
+          </li>
         </ul>
       </div>
     </div>
