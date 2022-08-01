6e1c4f678ffafb24283bfd4526009657969b3de1
NawImNate <jnhatch3@gmail.com>
2022-07-31 20:11:35 -0400

    import icon start

--- client_react/src/components/sidebar/Sidebar.jsx
+++ client_react/src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,16 @@
 import "./sidebar.css";
+import RssFeedIcon from "@mui/icons-material/RssFeed";
 
 const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem"></li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
