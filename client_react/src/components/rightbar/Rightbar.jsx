33573d3ec2f5da4d957acc6a46eeb959d5b9174f
NawImNate <jnhatch3@gmail.com>
2022-08-02 13:54:15 -0400

    mapping over users data to render Online component

--- client_react/src/components/rightbar/Rightbar.jsx
+++ client_react/src/components/rightbar/Rightbar.jsx
@@ -1,4 +1,6 @@
 import "./rightbar.css";
+import { Users } from "../../dummyData";
+import Online from "../online/Online";
 
 const Rightbar = () => {
   return (
@@ -17,39 +19,9 @@ const Rightbar = () => {
         <img className="rightbarAd" src="assets/ad.jpg" alt="pickleball ad" />
         <h4 className="rightbarTitle">Friends Online</h4>
         <ul className="rightbarFriendList">
-          <li className="rightbarFriend">
-            <div className="rightbarProfileImgContainer">
-              <img
-                className="rightbarProfileImg"
-                src="assets/person/zach.jpg"
-                alt="zach pic"
-              />
-              <span className="rightbarOnline"></span>
-            </div>
-            <span className="rightbarUsername">Zach Everett</span>
-          </li>
-          <li className="rightbarFriend">
-            <div className="rightbarProfileImgContainer">
-              <img
-                className="rightbarProfileImg"
-                src="assets/person/zach.jpg"
-                alt="zach pic"
-              />
-              <span className="rightbarOnline"></span>
-            </div>
-            <span className="rightbarUsername">Zach Everett</span>
-          </li>
-          <li className="rightbarFriend">
-            <div className="rightbarProfileImgContainer">
-              <img
-                className="rightbarProfileImg"
-                src="assets/person/zach.jpg"
-                alt="zach pic"
-              />
-              <span className="rightbarOnline"></span>
-            </div>
-            <span className="rightbarUsername">Zach Everett</span>
-          </li>
+          {Users.map((u) => {
+            return <Online key={u.id} user={u} />;
+          })}
         </ul>
       </div>
     </div>
