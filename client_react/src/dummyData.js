6e326c061dfdb67c6f419e5af1f2c3d84115cb8f
NawImNate <jnhatch3@gmail.com>
2022-08-01 20:32:03 -0400

    dummy data for users and posts, arry.

---
+++ client_react/src/dummyData.js
@@ -0,0 +1,112 @@
+export const users = [
+  {
+    id: 1,
+    profilePicture: "assets/person/1.jpg",
+    username: "Vardkes Arutyunov",
+  },
+  {
+    id: 2,
+    profilePicture: "assets/person/2.jpg",
+    username: "Noralie Iglesias",
+  },
+  {
+    id: 3,
+    profilePicture: "assets/person/3.jpg",
+    username: "Chris Aquino",
+  },
+  {
+    id: 4,
+    profilePicture: "assets/person/4.jpg",
+    username: "Zach Everett",
+  },
+  {
+    id: 5,
+    profilePicture: "assets/person/5.jpg",
+    username: "Ozni Tayang",
+  },
+  {
+    id: 6,
+    profilePicture: "assets/person/6.jpg",
+    username: "Alyona Hatchell",
+  },
+  {
+    id: 7,
+    profilePicture: "assets/person/7.jpg",
+    username: "Sandra Smajic",
+  },
+  {
+    id: 8,
+    profilePicture: "assets/person/8.jpg",
+    username: "Almir Smajic",
+  },
+];
+
+export const Posts = [
+  {
+    id: 1,
+    desc: "Love For All, Hatred For None.",
+    photo: "assets/post/1.jpg",
+    date: "5 mins ago",
+    userId: 1,
+    like: 32,
+    comment: 9,
+  },
+  {
+    id: 2,
+    photo: "assets/post/2.jpg",
+    date: "15 mins ago",
+    userId: 2,
+    like: 2,
+    comment: 1,
+  },
+  {
+    id: 3,
+    desc: "Every moment is a fresh beginning.",
+    photo: "assets/post/3.jpg",
+    date: "1 hour ago",
+    userId: 3,
+    like: 61,
+    comment: 2,
+  },
+  {
+    id: 4,
+    photo: "assets/post/4.jpg",
+    date: "4 hours ago",
+    userId: 4,
+    like: 7,
+    comment: 3,
+  },
+  {
+    id: 5,
+    photo: "assets/post/5.jpg",
+    date: "5 hours ago",
+    userId: 5,
+    like: 23,
+    comment: 5,
+  },
+  {
+    id: 6,
+    photo: "assets/post/6.jpg",
+    date: "1 day ago",
+    userId: 6,
+    like: 44,
+    comment: 6,
+  },
+  {
+    id: 7,
+    desc: "Never regret anything that made you smile.",
+    photo: "assets/post/7.jpg",
+    date: "2 days ago",
+    userId: 7,
+    like: 52,
+    comment: 3,
+  },
+  {
+    id: 8,
+    photo: "assets/post/8.jpg",
+    date: "3 days ago",
+    userId: 8,
+    like: 15,
+    comment: 1,
+  },
+];
