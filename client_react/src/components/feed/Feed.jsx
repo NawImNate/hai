347c9304b084e6388c2c95de67343da2758c6189
NawImNate <jnhatch3@gmail.com>
2022-08-01 21:11:20 -0400

    mapping over posts

--- client_react/src/components/feed/Feed.jsx
+++ client_react/src/components/feed/Feed.jsx
@@ -1,13 +1,16 @@
 import Share from "../share/Share";
 import Post from "../post/Post";
 import "./feed.css";
+import { Posts } from "../../dummyData";
 
 const Feed = () => {
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        <Post />
+        {Posts.map((p) => (
+          <Post key={p.id} post={p} />
+        ))}
       </div>
     </div>
   );
