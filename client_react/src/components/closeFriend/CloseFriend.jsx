576217dc4bdfce34338c93f0bdd9d4beb8bcc06f
NawImNate <jnhatch3@gmail.com>
2022-08-04 11:50:39 -0400

    functional component w/ dummy data

---
+++ client_react/src/components/closeFriend/CloseFriend.jsx
@@ -0,0 +1,16 @@
+import "./closefriend.css";
+
+function CloseFriend() {
+  return (
+    <li className="sidebarFriend">
+      <img
+        src="assets/person/zach.jpg"
+        alt="zach pic"
+        className="sidebarFriendImg"
+      />
+      <span className="sidebarFriendName">Zach Everett</span>
+    </li>
+  );
+}
+
+export default CloseFriend;
